756e4a152a3482d779a419a4a894a2c378ceab01
TerrifyIicI <137194109+TerrifyIicI@users.noreply.github.com>
2024-06-08 23:18:14 +0500

    add drawing grid, rhombus, square

--- GEMS.cpp
+++ GEMS.cpp
@@ -1,13 +1,7 @@
 ﻿#include <iostream>
-
-// GLEW
 #define GLEW_STATIC
 #include <GL/glew.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
-
-// Other includes
 #include "Shader.h"
 
 
@@ -15,24 +9,87 @@
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
 // Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
+const GLuint WIDTH = 600, HEIGHT = 800;
 
 class Renderer {
-public:
-    Renderer(GLuint VBO_, GLuint VAO_) : VBO(VBO_), VAO(VAO_) {
+
+
+private:
+    GLuint VBO, VAO;
+    float dx, dy;
+
+    void init(GLfloat* vertices, GLuint size) {
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+
+        // Position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);
+
         glBindVertexArray(VAO);
     }
 
-    void drawTriangle() {
+    void draw_line(int size) {
+        glDrawArrays(GL_LINE_LOOP, 0, size / (3 * sizeof(GLfloat)));
+        glBindVertexArray(0);
+    }
+
+public:
+
+    Renderer(GLuint VBO_, GLuint VAO_) : VBO(VBO_), VAO(VAO_) {
+       glBindVertexArray(VAO);
+        dx = 0, 5; dy = 0, 5;
+    }
+
+    void drawTriangle(GLfloat x, GLfloat y) {
+        x = x * dx - 1 + dx / 2;
+        y = y * dy - 1 + dy / 2;
+
         GLfloat vertices[] = {
             // Positions
-            0.5f, -0.5f, 0.0f,
-            -0.5f, -0.5f, 0.0f,
-            0.0f,  0.5f, 0.0f
+            x + dx / 2, y - dy / 2, 0.0f,
+            x - dx / 2, y - dy / 2, 0.0f,
+            x, y + dy / 2, 0.0f
+        };
+
+        init(vertices, sizeof(vertices));
+        glLineWidth(3);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+    }
+
+
+    void drawRhombus(GLfloat x, GLfloat y) {
+        x = x * dx - 1 + dx / 2;
+        y = y * dy - 1 + dy / 2;
+        GLfloat vertices[] = {
+            // Positions
+            x + dx / 2, y, 0.0f,
+            x, y + dy / 2, 0.0f,
+            x - dx / 2, y, 0.0f,
+            x, y - dy / 2, 0.0f,
         };
         init(vertices, sizeof(vertices));
         glLineWidth(3);
-        draw_triangle(sizeof(vertices));
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        glBindVertexArray(0);
+    }
+
+
+    void drawSquare(GLfloat x, GLfloat y) {
+        x = x * dx - 1 + dx / 2;
+        y = y * dy - 1 + dy / 2;
+        GLfloat vertices[] = {
+            // Positions
+            x - dx / 2, y - dy / 2, 0.0f,
+            x + dx / 2, y - dy / 2, 0.0f,
+            x + dx / 2, y + dy / 2, 0.0f,
+            x - dx / 2, y + dy / 2, 0.0f,
+        };
+        init(vertices, sizeof(vertices));
+        glLineWidth(3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+        glBindVertexArray(0);
     }
 
     void drawBorderLines() {
@@ -48,32 +105,43 @@ public:
         draw_line(sizeof(vertices));
     }
 
+    void drawGrid(int x, int y) {
+        dx = 2.0f / (x + 2);
+        dy = 2.0f / (y + 3);
 
-    void init(GLfloat* vertices, GLuint size) {
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, size, vertices, GL_STATIC_DRAW);
+        // Рисуем вертикальные линии
+        float y1 = -1.0f + dy;
+        float y2 = 1.0f - 2 * dy;
+        for (int i = 1; i <= x + 1; i++) {
+            float x1 = -1.0f + dx * i;
+            float x2 = x1;
 
-        // Position attribute
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-        glEnableVertexAttribArray(0);
+            GLfloat vertices[] = {
+                x1, y1, 0.0f,
+                x2, y2, 0.0f
+            };
+            init(vertices, sizeof(vertices));
+            glDrawArrays(GL_LINES, 0, 2);
+        }
 
-        glBindVertexArray(VAO);
+        // Рисуем горизонтальные линии
+        float x1 = -1.0f + dx;
+        float x2 = 1.0f - dx;
+        for (int i = 1; i <= y + 1; i++) {
+            float y1 = -1.0f + dy * i;
+            float y2 = y1;
+
+            GLfloat vertices[] = {
+                x1, y1, 0.0f,
+                x2, y2, 0.0f
+            };
+            init(vertices, sizeof(vertices));
+            glDrawArrays(GL_LINES, 0, 2);
+        }
     }
-
-
-    void draw_line(int size) {
-        glDrawArrays(GL_LINE_LOOP, 0, size / (3 * sizeof(GLfloat)));
-        glBindVertexArray(0);
-    }
-
-    void draw_triangle(int size) {
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
-    }
-
-private:
-    GLuint VBO, VAO;
 };
+
+
 
 // The MAIN function, from here we start the application and run the game loop
 int main()
@@ -105,11 +173,9 @@ int main()
     // Build and compile our shader program
     Shader ourShader("shaders/default.vs", "shaders/default.frag");
     GLuint VBO, VAO;
-
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     Renderer ren = Renderer(VBO, VAO);
-
 
     // Game loop
     while (!glfwWindowShouldClose(window))
@@ -124,12 +190,21 @@ int main()
 
         // Draw the triangle
         ourShader.Use();
-        ren.drawTriangle();
+        ren.drawGrid(6, 4);
         ren.drawBorderLines();
+        ren.drawSquare(1, 3);
+        ren.drawRhombus(3, 4);
+        ren.drawTriangle(5, 2);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
     }
+
+    // Properly de-allocate all resources once they've outlived their purpose
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    // Terminate GLFW, clearing any resources allocated by GLFW.
+    glfwTerminate();
     return 0;
 }
 
@@ -139,3 +214,4 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
+
