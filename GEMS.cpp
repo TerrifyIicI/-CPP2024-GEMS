26f6ade97410bb7eea750902e2be78c2262aa37e
TerrifyIicI <137194109+TerrifyIicI@users.noreply.github.com>
2024-06-08 23:26:37 +0500

    add figure ellipse and figure of constant width

--- GEMS.cpp
+++ GEMS.cpp
@@ -1,15 +1,22 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "Shader.h"
+#include <math.h>
 
 
 // Function prototypes
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+// глобальная переменная для хранения индексов квадрата сетки
+int x_index, y_index;
+// обработчик события нажатия мыши
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 
 // Window dimensions
-const GLuint WIDTH = 600, HEIGHT = 800;
+GLuint WIDTH = 600, HEIGHT = 800;
+const int x_parts = 5, y_parts = 7;
 
 class Renderer {
 
@@ -37,7 +44,7 @@ private:
 public:
 
     Renderer(GLuint VBO_, GLuint VAO_) : VBO(VBO_), VAO(VAO_) {
-       glBindVertexArray(VAO);
+        glBindVertexArray(VAO);
         dx = 0, 5; dy = 0, 5;
     }
 
@@ -75,15 +82,17 @@ public:
         glBindVertexArray(0);
     }
 
-
     void drawSquare(GLfloat x, GLfloat y) {
         x = x * dx - 1 + dx / 2;
         y = y * dy - 1 + dy / 2;
+
+
+        // Рисуем квадрат
         GLfloat vertices[] = {
             // Positions
-            x - dx / 2, y - dy / 2, 0.0f,
+            x - dx / 2, y - dy / 2, 1.0f,
             x + dx / 2, y - dy / 2, 0.0f,
-            x + dx / 2, y + dy / 2, 0.0f,
+            x + dx / 2, y + dy / 2, 1.0f,
             x - dx / 2, y + dy / 2, 0.0f,
         };
         init(vertices, sizeof(vertices));
@@ -91,6 +100,7 @@ public:
         glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
         glBindVertexArray(0);
     }
+
 
     void drawBorderLines() {
         GLfloat vertices[] = {
@@ -139,8 +149,71 @@ public:
             glDrawArrays(GL_LINES, 0, 2);
         }
     }
-};
 
+    void FillEllipse(GLfloat x, GLfloat y) {
+        const int  pointCount = 10;
+        const float step = float(2 * M_PI) / pointCount;
+        x = x * dx - 1 + dx / 2;
+        y = y * dy - 1 + dy / 2;
+
+        GLfloat vertices[pointCount * 2 * 3]; // 2 вершины на отрезок, 3 координаты на вершину
+        for (int i = 0; i < pointCount; i++) {
+            float angle = i * step;
+            float x1 = dx / 2 * cosf(angle) + x;
+            float y1 = dy / 2 * sinf(angle) + y;
+            float x2 = dx / 2 * cosf(angle + step) + x;
+            float y2 = dy / 2 * sinf(angle + step) + y;
+
+            vertices[i * 6] = x1;
+            vertices[i * 6 + 1] = y1;
+            vertices[i * 6 + 2] = 0.0f;
+
+            vertices[i * 6 + 3] = x2;
+            vertices[i * 6 + 4] = y2;
+            vertices[i * 6 + 5] = 0.0f;
+        }
+
+        init(vertices, sizeof(vertices));
+        glLineWidth(3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, pointCount * 2);
+        glBindVertexArray(0);
+    }
+
+    void FillFigureOfConstantWidth(GLfloat x, GLfloat y) {
+        const int pointCount = 360;
+        const float step = float(2 * M_PI) / pointCount;
+
+        x = x * dx - 1 + dx / 2;
+        y = y * dy - 1 + dy / 2;
+
+        GLfloat vertices[pointCount * 2 * 3]; // 2 вершины на отрезок, 3 координаты на вершину
+        for (int i = 0; i < pointCount; i++) {
+            float t = i * step;
+            float p = 9 + cosf(3 * t);
+            float dp = -3 * sinf(3 * t);
+
+            float x1 = p * cosf(t) - dp * sinf(t) + x;
+            float y1 = p * sinf(t) + dp * cosf(t) + y;
+            float x2 = p * cosf(t + step) - dp * sinf(t + step) + x;
+            float y2 = p * sinf(t + step) + dp * cosf(t + step) + y;
+
+            vertices[i * 6] = x1 * dx / 20 + x;
+            vertices[i * 6 + 1] = y1 * dy / 20 + y;
+            vertices[i * 6 + 2] = 0.0f;
+
+            vertices[i * 6 + 3] = x2 * dx / 20 + x;
+            vertices[i * 6 + 4] = y2 * dy / 20 + y;
+            vertices[i * 6 + 5] = 0.0f;
+        }
+
+        init(vertices, sizeof(vertices));
+        glLineWidth(3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, pointCount * 2);
+        glBindVertexArray(0);
+    }
+
+
+};
 
 
 // The MAIN function, from here we start the application and run the game loop
@@ -160,6 +233,7 @@ int main()
 
     // Set the required callback functions
     glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
     // Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
     glewExperimental = GL_TRUE;
@@ -190,11 +264,15 @@ int main()
 
         // Draw the triangle
         ourShader.Use();
-        ren.drawGrid(6, 4);
+        ren.drawGrid(x_parts, y_parts);
         ren.drawBorderLines();
         ren.drawSquare(1, 3);
         ren.drawRhombus(3, 4);
         ren.drawTriangle(5, 2);
+        ren.FillEllipse(4, 3);
+        ren.FillFigureOfConstantWidth(3, 6);
+
+        ren.drawGrid(x_parts, y_parts);
 
         // Swap the screen buffers
         glfwSwapBuffers(window);
@@ -213,5 +291,23 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+// обработчик события нажатия мыши
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        // получаем текущие координаты курсора мыши
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+
+        // определяем, в каком квадрате сетки находится курсор мыши
+        x_index = (int)(xpos / (WIDTH / (x_parts + 2)));
+        y_index = (int)(ypos / (HEIGHT / (y_parts + 3)));
+
+        // выводим надпись с индексами квадрата сетки
+        std::cout << "Mouse is in square: " << x_index << ", " << y_index << std::endl;
+    }
 }
 
